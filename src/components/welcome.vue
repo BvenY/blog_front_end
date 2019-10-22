09d0d07cc7789788cb8693d9a5c0ae34f88d1a8e
Yangbowen <bestbven@163.com>
2019-10-22 22:38:43 +0800

    welcome

---
+++ src/components/welcome.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="welcome">
+    <div class="main">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'welcome',
+  data () {
+    return {
+    
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .welcome{
+    width: 100%;
+    height: 100%;
+    background-image:url(../assets/img/welcome.jpg);
+    background-size:100% 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .main{
+    width: 40%;
+    height: 50%;
+    background-color: rgba(255,255,255,0.2);
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import welcome from '@/components/welcome'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'welcome',
+      component: welcome
     }
   ]
 })
